965f5dbb4f58350cb8923cf88957c2186ff8aa2d
Christian <christianeda@Gmail.com>
2022-01-06 20:53:19 +0100

    mein erster commit & push in jetbrains

--- file1.py
+++ file1.py
@@ -1,4 +1,4 @@
-def abrechnung(einkauf, mwst = .2):
+def abrechnung(einkauf, mwst=.2):
     '''
 
     Diese Funktion berechnet die Gesamtsumme des EInkaufs
@@ -16,15 +16,15 @@ def abrechnung(einkauf, mwst = .2):
     summe *= 1. + mwst
     return summe
 
-#Dieser kleine Test überprüft, ob wir usn in dem Hauptteil des Programms befinden,
-#oder als Bibliothek verwendet werden. Im Falle der Bibliothek soll nämlich keine
-#Funktion ausgeführt werden; Das wäre störend.
+
+# Dieser kleine Test überprüft, ob wir usn in dem Hauptteil des Programms befinden,
+# oder als Bibliothek verwendet werden. Im Falle der Bibliothek soll nämlich keine
+# Funktion ausgeführt werden; Das wäre störend.
 
 
-if __name__=='__main__':
-    print(__name__) #das ist "file1" in der Datei "wifi.py", wenn als Library ausgeführt
-    a=abrechnung([5.6,1,9])
+if __name__ == '__main__':
+    print(__name__)  # das ist "file1" in der Datei "wifi.py", wenn als Library ausgeführt
+    a = abrechnung([5.6, 1, 9])
     print(a)
 
-
-
+# test
